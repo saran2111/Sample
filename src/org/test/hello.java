591c4e4b116a090383e522fa912e0b7d9005f111
vetrikrrish <vetrikrrish@gmail.com>
2019-05-07 14:03:52 +0530

    added my project

---
+++ src/org/test/hello.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class hello {
+public void empId() {
+System.out.println("p14571");
+}
+	public void empName() {
+System.out.println("saran");
+	}
+	public static void main(String[] args) {
+	hello h=new hello();
+	h.empId();
+	h.empName();
+	}
+}
